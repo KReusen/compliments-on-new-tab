dfe357b96998337e5f8a9bba14e96f6e15f51e5a
Kees Reusen <k.reusen@gmail.com>
2018-11-04 10:14:34 +0100

    added caching of quotes by one day

--- chrome_extension/compliment_getter.js
+++ chrome_extension/compliment_getter.js
@@ -1,25 +1,49 @@
-var sheet_id = "";
-
-function pick_compliment(json_response) {
-  var all_compliments = json_response.feed.entry;
-  var total_compliments = all_compliments.length;
-  var random = Math.floor(Math.random() * total_compliments);
-  return all_compliments[random].title.$t;
+function parse_quotes_from_web(json_response) {
+  var data = json_response.feed.entry;
+  var all_quotes = data.map(x => x.title.$t);
+  cache_quotes(all_quotes);
+  return all_quotes;
 }
 
-function get_compliment() {
-  const url = `https://spreadsheets.google.com/feeds/list/${sheet_id}/od6/public/values?alt=json`;
+function get_random_quote(quotes) {
+  var total_quotes = quotes.length;
+  var random = Math.floor(Math.random() * total_quotes);
+  return quotes[random];
+}
+
+function cache_quotes(quotes) {
+  localStorage.setItem("last_updated", JSON.stringify(new Date()));
+  localStorage.setItem("quotes", JSON.stringify(quotes));
+}
+
+function get_quote_from_web() {
+  const url =
+    "https://spreadsheets.google.com/feeds/list/1_IJg6jPTlo0e3KeV-D4svnOV-G1DKP2a6wE7LqxhBuU/od6/public/values?alt=json";
   return fetch(url)
     .then(function(response) {
       return response.json();
     })
     .then(function(json_response) {
-      return pick_compliment(json_response);
+      return parse_quotes_from_web(json_response);
+    })
+    .then(function(all_quotes) {
+      return get_random_quote(all_quotes);
     });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  get_compliment().then(function(response) {
-    document.getElementById("complimentText").innerHTML = response;
-  });
+  var last_updated = localStorage.getItem("last_updated");
+  var hours_passed = Math.abs(new Date() - JSON.parse(last_updated)) / 36e5;
+
+  if (hours_passed > 24) {
+    get_quote_from_web().then(function(response) {
+      document.getElementById("quoteText").innerHTML = response;
+    });
+    console.log("got quote from web");
+  } else {
+    var quotes = JSON.parse(localStorage.getItem("quotes"));
+    var random_quote = get_random_quote(quotes);
+    document.getElementById("quoteText").innerHTML = random_quote;
+    console.log("got quote from cache");
+  }
 });
